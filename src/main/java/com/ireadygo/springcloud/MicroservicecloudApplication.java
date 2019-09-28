d65a1154e12c0f12895b0fdba61643a2d31c2fea
maoqinglin <linmq@snailgame.net>
2019-09-28 12:00:18 +0800

    初始化微服务主工程

---
+++ src/main/java/com/ireadygo/springcloud/MicroservicecloudApplication.java
@@ -0,0 +1,13 @@
+package com.ireadygo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicecloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicecloudApplication.class, args);
+    }
+
+}
