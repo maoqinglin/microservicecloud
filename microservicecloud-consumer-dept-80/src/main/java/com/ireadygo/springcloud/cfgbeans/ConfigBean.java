ad7e45e95ceb58ef3147902cbd58532654040995
maoqinglin <linmq@snailgame.net>
2019-09-29 20:01:39 +0800

    修复添加Ribbon依赖后，rest 接口返回xml格式数据问题

--- microservicecloud-consumer-dept-80/src/main/java/com/ireadygo/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/ireadygo/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.ireadygo.springcloud.cfgbeans;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,7 +8,9 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ConfigBean {
 
+//    开启ribbon负载均衡，客户端负载均衡
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
--- microservicecloud-consumer-dept-80/src/main/java/com/ireadygo/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/ireadygo/springcloud/controller/DeptController_Consumer.java
@@ -2,6 +2,7 @@ package com.ireadygo.springcloud.controller;
 
 import com.ireadygo.springcloud.entities.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,14 @@ import java.util.List;
 @RestController
 public class DeptController_Consumer {
 
-    public static final String REST_URL_PREFIX = "http://localhost:8001";
+//    public static final String REST_URL_PREFIX = "http://localhost:8001"; 不需要端口
+    public static final String REST_URL_PREFIX = "http://microservicecloud-dept";
 
     @Autowired
     RestTemplate restTemplate;
 
-    @RequestMapping("/consumer/dept/get/{id}")
+//    使用Ribbon负载均衡后，会依赖jackson-dataformat-xml这个依赖，导致返回数据为xml
+    @RequestMapping(value = "/consumer/dept/get/{id}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Dept get(@PathVariable("id") Long id) {
         return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
     }
@@ -27,7 +30,7 @@ public class DeptController_Consumer {
         return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
     }
 
-    @RequestMapping("/consumer/dept/list")
+    @RequestMapping(value = "/consumer/dept/list", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Dept> list() {
         return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
     }
